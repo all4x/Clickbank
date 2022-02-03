c41c98fdaa60fa3f58473654940d27b966aba1cb
all4x <estudar0101@gmail.com>
2022-02-03 17:41:03 -0300

    ta

--- components/Topbar/index.jsx
+++ components/Topbar/index.jsx
@@ -9,7 +9,7 @@ const index = () => {
         <nav class=" bg-gray-100">
         <div class="flex justify-center items-center bg-red-500 py-6 lg:px-40 md:px-20 px-10">
             <div class="">
-            <a href="#"><Image class="h-10 cursor-pointer" src={Java} alt="" /></a>
+            <a href="#"> <h1 className='text-3xl'>sem nome</h1></a>
             </div>
         </div>
         </nav>
---
+++ pages/index.jsx
@@ -0,0 +1,45 @@
+import Head from 'next/head'
+import Footer from '../components/Footer'
+import Topbar from '../components/Topbar'
+import Alle from '../public/alle.jpg'
+import Image from 'next/image'
+import Link from 'next/link'
+
+
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <meta name="title" content="Java Man ©"/>
+        <link rel="icon" href="/" />
+      </Head>
+      <Topbar/>
+      <main className="">
+      
+        
+          {/* BUTTON CTA */} 
+          <div className='text-left'>
+            <p className='p-5 text-2xl'>Titulo, não tem titulo :)</p>
+            <div className='flex justify-center pt-5 mb-5'>
+             <Link href="/mask">  
+                  <button className="p-2 pl-16 pr-16 bg-yellow-500 text-gray-900 font-semibold text-lg rounded-lg focus:border-4 border-gray-300">botão aleatorio fodase</button>
+             </Link>
+            </div> 
+            <div className='flex items-center justify-center mb-9 animate-spin' >
+            <Image src={Alle}/>
+              <p>Gira vagabunda</p>
+            </div>
+
+          </div>  
+              
+      </main>
+      <footer className=''>
+      <Footer/>
+      </footer>
+    
+    
+    </div>
+  )
+}
+
