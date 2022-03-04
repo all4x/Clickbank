b410358349503b0b39b37e2aa9cad4c95651b8f4
all4x <estudar0101@gmail.com>
2022-03-04 19:27:25 -0300

    new page-2

--- pages/index.jsx
+++ pages/index.jsx
@@ -23,14 +23,16 @@ export default function Home() {
             <p className=' bg-yellow-400 p-2 text-2xl text-center  font-extrabold'>For Over 80% OFF Today!</p>
 
            
-            <div className='flex justify-center pt-5 mb-5'>
-             <Link href="https://bit.ly/3twIfMp">  
-              <button className=" pl-16 pr-16 brightness-110 bg-blue-500 text-gray-50 font-semibold text-lg rounded-lg focus:border-4 border-gray-300">Take Me To The Discount Page</button>
-             </Link>
-            </div> 
+            
             <div className='flex items-center justify-center mb-2 p-5 ' >
             <Image src={Introducing}/>
+            
             </div>
+            <div className='flex justify-center pt-3 mb-3'>
+             <Link href="https://bit.ly/3twIfMp">  
+              <button className=" pl-10 pr-10 brightness-110 bg-blue-500 text-gray-50 font-semibold text-lg rounded-lg focus:border-4 border-gray-300">Take Me To The Discount Page</button>
+             </Link>
+            </div> 
             <div className='bg-black'>
             <div className='flex justify-center p-4'>
             <Image src={Check}/>
